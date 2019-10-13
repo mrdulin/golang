f306735937e7cd17dfbed3cc33c8318a82421746
Lin L Du <novaline.dulin@gmail.com>
2019-10-13 22:36:36 +0800

    improve sample

--- src/22-channels/02-dead-lock/main.go
+++ src/22-channels/02-dead-lock/main.go
@@ -1,6 +1,21 @@
 package main
 
+import "fmt"
+
+// cannot recover from a deadlock.
+// https://stackoverflow.com/questions/14075382/how-do-i-catch-the-exception-of-a-channel-deadlocking
+// https://stackoverflow.com/questions/16783040/how-to-recover-the-deadlock-error-in-golang?noredirect=1&lq=1
+// deadlock is not a panic error, it's a fatal error, see https://golang.org/pkg/log/#Fatal
 func main() {
+	defer func() {
+		r := recover()
+		fmt.Printf("rrrr=%v\n", r)
+		if r != nil {
+			fmt.Printf("r=%v\n", r)
+		}
+	}()
+
 	ch := make(chan int)
 	ch <- 5
+	fmt.Println("code will never arrive here")
 }
