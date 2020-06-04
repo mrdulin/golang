27eb9af13f9dc047da1afa6f75dfa333f7a1a5aa
mrdulin <novaline.dulin@gmail.com>
2020-06-04 18:29:16 +0800

    add concurrency safe testing sample

--- src/testing/concurrency/main_test.go
+++ src/testing/concurrency/main_test.go
@@ -7,6 +7,11 @@ import (
 )
 
 func TestUser_DeleteTask(t *testing.T) {
+  t.Skip()
+  var (
+    nOps = 100
+    wg sync.WaitGroup
+  )
   tasks := map[string]string{
     "1": "a",
     "2": "b",
@@ -14,7 +19,15 @@ func TestUser_DeleteTask(t *testing.T) {
   u := &main.User{
     Tasks: tasks,
   }
-  u.DeleteTask("1")
+  wg.Add(nOps)
+  for i := 0; i < nOps; i++ {
+    go func() {
+      defer wg.Done()
+      u.DeleteTask("1")
+    }()
+  }
+  wg.Wait();
+  
   if u.Tasks["1"] != "" {
     t.Errorf("task with taskId 1 should be deleted, got: %s", u.Tasks["1"])
   }
@@ -37,5 +50,27 @@ func TestSafeCounter_Inc(t *testing.T) {
   
   if counter.Value("a") != nOps {
     t.Errorf("the value of key a should be %d, got: %d", nOps, counter.Value("a"))
+  }
+}
+
+
+func TestSafeCounter_Value(t *testing.T) {
+  var (
+    nOps = 100
+    wg sync.WaitGroup
+  )
+  counter := main.SafeCounter{V: make(map[string]int)}
+  wg.Add(nOps)
+  for i := 0; i < nOps; i++ {
+    go func() {
+      defer wg.Done()
+      if counter.Value("a") == 0 {
+        counter.Inc("a")
+      }
+    }()
+  }
+  wg.Wait()
+  if counter.Value("a") != 1 {
+    t.Errorf("the value of key a should be %d, got: %d", 1, counter.Value("a"))
   }
 }
